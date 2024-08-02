15a1f77c54083f1aaa44b0e06010aed38fdb3c86
w0wayush <ayupandey82@gmail.com>
2024-08-02 18:07:50 +0530

    Done with Contact Details Form

---
+++ src/app/store/slices/formSlice.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface FormState {
+  step: number;
+}
+
+const initialState: FormState = {
+  step: 1, // Initialize at step 1
+};
+
+const formSlice = createSlice({
+  name: "form",
+  initialState,
+  reducers: {
+    setStep(state, action: PayloadAction<number>) {
+      state.step = action.payload;
+    },
+    nextStep(state) {
+      if (state.step < 5) {
+        state.step += 1;
+      }
+    },
+    prevStep(state) {
+      if (state.step > 1) {
+        state.step -= 1;
+      }
+    },
+  },
+});
+
+export const { setStep, nextStep, prevStep } = formSlice.actions;
+export default formSlice.reducer;
---
+++ src/lib/data/contactDetailData.ts
@@ -0,0 +1,9 @@
+const countryPhoneCode = [
+  { code: "+1", country: "United States" },
+  { code: "+44", country: "United Kingdom" },
+  { code: "+91", country: "India" },
+  { code: "+81", country: "Japan" },
+  { code: "+61", country: "Australia" },
+];
+
+export default countryPhoneCode;
